35b127bb3d29e1baaf3ab2044bba957fc19dc9b6
adinathauti <adinathauti@gmail.com>
2017-06-16 09:44:35 +0000

    Done

--- build.py
+++ build.py
@@ -1,2 +1,16 @@
 def solution(dic1, dic2):
-    """Enter code here"""
+    k1=dic1.keys()
+    k2=dic2.keys()
+    v1 = dic1.values()
+    v2 = dic2.values()
+    k1.extend(k2)
+    v1.extend(v2)
+    #print k1
+    #print v1
+    ts = zip(k1,v1)
+    #print ts
+    dz=dict(ts)
+    return dz
+
+
+solution({1: 10, 2: 20}, {3: 30, 4: 40})
