96e5783646828c1e024c97a7b9f3685c9277d558
bhattbhavesh91 <bhattbhavesh91@gmail.com>
2017-06-16 05:05:00 +0000

    Done

--- build.py
+++ build.py
@@ -1,2 +1,6 @@
 def solution(dic1, dic2):
     """Enter code here"""
+    dic3 = dict(dic1) #Creating a dictionary with dic1 as an input and the dict construct
+    dic3.update(dic2) #Updating the dictionary
+
+    return dic3
