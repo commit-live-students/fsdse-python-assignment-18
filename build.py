47e6448c903de5b11719601b23221df540d348d8
rishabh-github <rishabh-github@users.noreply.github.com>
2017-06-18 17:26:43 +0000

    Done

--- build.py
+++ build.py
@@ -1,2 +1,7 @@
 def solution(dic1, dic2):
-    """Enter code here"""
+    dic = {}
+    for d in [dic1,dic2]:
+        dic.update(d)
+    return dic
+
+print solution({1: 10, 2: 20}, {3: 30, 4: 40})
