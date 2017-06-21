3c1d6ac35e755dec1ceb0f7af423a992cb93bfd8
ramkrishan-sahu <ram04190@gmail.com>
2017-06-21 08:34:44 +0000

    Done

--- build.py
+++ build.py
@@ -1,2 +1,15 @@
-def solution(dic1, dic2):
-    """Enter code here"""
+# Concatenate following dictionaries to create a new one.
+# https://app.commit.live/lesson/fsdse-python-assignment-18
+
+# a = {1: 10, 2: 20}
+# b = {3: 30, 4: 40}
+
+#a = dict(a.items() + b.items())
+#OR
+def solution(a, b):
+    a.update(b)
+    print a
+    return a
+
+# dic(a, b)
+#DOne, Not Posted =================================================================================
